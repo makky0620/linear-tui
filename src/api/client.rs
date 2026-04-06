c979fa4e2d3c64ec079ea8e47df6ae844b3894b0
Takashi Makino <7555074+makky0620@users.noreply.github.com>
2026-04-06 16:49:07 +0900

    fix(api): use Float! for setting estimate, inline null for clearing

--- src/api/client.rs
+++ src/api/client.rs
@@ -518,20 +518,32 @@ impl LinearClient {
             #[serde(rename = "issueUpdate")]
             _issue_update: MutationSuccess,
         }
-        let variables = serde_json::json!({
-            "id": issue_id,
-            "estimate": estimate,
-        });
-        let _: Resp = self
-            .query(
-                r#"mutation($id: String!, $estimate: Float) {
-                    issueUpdate(id: $id, input: { estimate: $estimate }) {
-                        success
-                    }
-                }"#,
-                Some(variables),
-            )
-            .await?;
+        let _: Resp = match estimate {
+            Some(val) => {
+                let variables = serde_json::json!({ "id": issue_id, "estimate": val });
+                self.query(
+                    r#"mutation($id: String!, $estimate: Float!) {
+                        issueUpdate(id: $id, input: { estimate: $estimate }) {
+                            success
+                        }
+                    }"#,
+                    Some(variables),
+                )
+                .await?
+            }
+            None => {
+                let variables = serde_json::json!({ "id": issue_id });
+                self.query(
+                    r#"mutation($id: String!) {
+                        issueUpdate(id: $id, input: { estimate: null }) {
+                            success
+                        }
+                    }"#,
+                    Some(variables),
+                )
+                .await?
+            }
+        };
         Ok(())
     }
 
